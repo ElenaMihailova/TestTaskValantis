d6b996afa5a48e918a01bd8d4972d68a3492ed39
Elena <pumpkinpum@gmail.com>
2024-02-29 22:15:41 +0300

    feat: add price filter

--- src/components/blocks/filter/filter.jsx
+++ src/components/blocks/filter/filter.jsx
@@ -1,34 +1,16 @@
-import {useState} from 'react';
 import FilterBrand from './filterBrand';
 import FilterName from './filterName';
+import FilterPrice from './filterPrice';
 
 import * as Styled from './style';
 
 function Filter() {
-
   return (
     <Styled.Wrapper>
       <Styled.FieldWrapper>
         <FilterBrand />
         <FilterName />
-
-        {/* {Object.entries(inputState).map(
-          ([id, isfilled]) =>
-            id !== 'brand' && (
-              <Styled.Field key={id}>
-                <Styled.Input
-                  type={id === 'price' ? 'number' : 'text'}
-                  id={id}
-                  onChange={handleInputChange}
-                  onFocus={handleInputFocus}
-                  onBlur={handleInputBlur}
-                />
-                <Styled.Label htmlFor={id} isfilled={isfilled.toString()}>
-                  {id === 'name' ? 'Название' : 'Цена'}
-                </Styled.Label>
-              </Styled.Field>
-            )
-        )} */}
+        <FilterPrice />
       </Styled.FieldWrapper>
     </Styled.Wrapper>
   );
---
+++ src/components/blocks/filter/filterPrice.jsx
@@ -0,0 +1,35 @@
+import {useState, useContext, useCallback} from 'react';
+import {ProductDataContext} from '../../dataProviders/productDataProviders';
+import useDebounce from '../../helpers/useDebounce';
+
+import * as Styled from './style';
+
+function FilterPrice() {
+  const {filterProducts} = useContext(ProductDataContext);
+  const [filterValue, setFilterValue] = useState('');
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputChange = useCallback(
+    (value) => {
+      setFilterValue(value.trim());
+      const numericFilterValue = parseFloat(value.trim());
+      filterProducts('price', numericFilterValue);
+      setIsFilled(value.trim() !== '');
+    },
+    [filterProducts]
+  );
+
+  const debouncedHandleInputChange = useDebounce(handleInputChange, 500);
+
+  const handleChange = (event) =>
+    debouncedHandleInputChange(event.target.value);
+
+  return (
+    <Styled.Field>
+      <Styled.Input type='number' onChange={handleChange} />
+      <Styled.Label isfilled={isFilled.toString()}>Цена</Styled.Label>
+    </Styled.Field>
+  );
+}
+
+export default FilterPrice;
